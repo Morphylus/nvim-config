9cde65ae0df939d676bceb8d4ca45508ff7ba84d
Morphylus <martindefuns@kns.ch>
2025-02-07 18:49:52 +0100

    initial nvim config

---
+++ lua/funsi/init.lua
@@ -0,0 +1,1 @@
+require("funsi.lazy_init")
---
+++ lua/funsi/plugins/nvim-tree.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+	-- disable netrw
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
+
+	-- Enable 24-bit color
+	vim.opt.termguicolors = true
+    require("nvim-tree").setup {}
+
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
+  end,
+}
