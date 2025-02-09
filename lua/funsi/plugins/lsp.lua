cd38acea80c8217db2905c9eb3b8ddbbb9cf135b
Morphylus <martindefuns@kns.ch>
2025-02-09 16:35:44 +0100

    add descriptions to lsp commands

--- lua/funsi/plugins/lsp.lua
+++ lua/funsi/plugins/lsp.lua
@@ -1,129 +1,133 @@
-return {{
+return { {
     'williamboman/mason.nvim',
     lazy = false,
     opts = {}
 }, -- Autocompletion
-{
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    config = function()
-        local cmp = require('cmp')
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        config = function()
+            local cmp = require('cmp')
 
-        cmp.setup({
-            sources = {{
-                name = 'nvim_lsp'
-            }},
-            mapping = cmp.mapping.preset.insert({
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            cmp.setup({
+                sources = { {
+                    name = 'nvim_lsp'
+                } },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-                -- Use enter to confirm completion
-                ['<CR>'] = cmp.mapping.confirm({
-                    select = false
-                }),
+                    -- Use enter to confirm completion
+                    ['<CR>'] = cmp.mapping.confirm({
+                        select = false
+                    }),
 
-                -- Navigate completions with <C-j> and <C-k>
-                ["<C-j>"] = cmp.mapping.select_next_item({
-                    behavior = cmp.SelectBehavior.Insert
-                }),
-                ["<C-k>"] = cmp.mapping.select_prev_item({
-                    behavior = cmp.SelectBehavior.Insert
-                })
-            }),
-            snippet = {
-                expand = function(args)
-                    vim.snippet.expand(args.body)
-                end
-            }
-        })
-    end
-}, -- LSP
-{
-    'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {{'hrsh7th/cmp-nvim-lsp'}, {'williamboman/mason.nvim'}, {'williamboman/mason-lspconfig.nvim'}},
-    init = function()
-        -- Reserve a space in the gutter
-        -- This will avoid an annoying layout shift in the screen
-        vim.opt.signcolumn = 'yes'
-    end,
-    config = function()
-        local lsp_defaults = require('lspconfig').util.default_config
-
-        -- Add cmp_nvim_lsp capabilities settings to lspconfig
-        -- This should be executed before you configure any language server
-        lsp_defaults.capabilities = vim.tbl_deep_extend('force', lsp_defaults.capabilities,
-            require('cmp_nvim_lsp').default_capabilities())
-
-        -- Define the autoformat function locally
-        local buffer_autoformat = function(bufnr)
-            local group = 'lsp_autoformat'
-            vim.api.nvim_create_augroup(group, {
-                clear = false
-            })
-            vim.api.nvim_clear_autocmds({
-                group = group,
-                buffer = bufnr
-            })
-
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                buffer = bufnr,
-                group = group,
-                desc = 'LSP format on save',
-                callback = function()
-                    -- note: do not enable async formatting
-                    vim.lsp.buf.format({
-                        async = false,
-                        timeout_ms = 10000
+                    -- Navigate completions with <C-j> and <C-k>
+                    ["<C-j>"] = cmp.mapping.select_next_item({
+                        behavior = cmp.SelectBehavior.Insert
+                    }),
+                    ["<C-k>"] = cmp.mapping.select_prev_item({
+                        behavior = cmp.SelectBehavior.Insert
                     })
-                end
+                }),
+                snippet = {
+                    expand = function(args)
+                        vim.snippet.expand(args.body)
+                    end
+                }
             })
         end
+    }, -- LSP
+    {
+        'neovim/nvim-lspconfig',
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = { { 'hrsh7th/cmp-nvim-lsp' }, { 'williamboman/mason.nvim' }, { 'williamboman/mason-lspconfig.nvim' } },
+        init = function()
+            -- Reserve a space in the gutter
+            -- This will avoid an annoying layout shift in the screen
+            vim.opt.signcolumn = 'yes'
+        end,
+        config = function()
+            local lsp_defaults = require('lspconfig').util.default_config
 
-        -- LspAttach is where you enable features that only work
-        -- if there is a language server active in the file
-        vim.api.nvim_create_autocmd('LspAttach', {
-            desc = 'LSP actions',
-            callback = function(event)
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            -- This should be executed before you configure any language server
+            lsp_defaults.capabilities = vim.tbl_deep_extend('force', lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities())
 
-                -- Autoformat on save
-                local id = vim.tbl_get(event, 'data', 'client_id')
-                local client = id and vim.lsp.get_client_by_id(id)
-                if client == nil then
-                    return
-                end
+            -- Define the autoformat function locally
+            local buffer_autoformat = function(bufnr)
+                local group = 'lsp_autoformat'
+                vim.api.nvim_create_augroup(group, {
+                    clear = false
+                })
+                vim.api.nvim_clear_autocmds({
+                    group = group,
+                    buffer = bufnr
+                })
 
-                if client.supports_method('textDocument/formatting') then
-                    buffer_autoformat(event.buf)
-                end
-
-                local opts = {
-                    buffer = event.buf
-                }
-
-                vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-                vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-                vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-                vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-                vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-                vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-                vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-                vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-                vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                vim.api.nvim_create_autocmd('BufWritePre', {
+                    buffer = bufnr,
+                    group = group,
+                    desc = 'LSP format on save',
+                    callback = function()
+                        -- note: do not enable async formatting
+                        vim.lsp.buf.format({
+                            async = false,
+                            timeout_ms = 10000
+                        })
+                    end
+                })
             end
-        })
 
-        require('mason-lspconfig').setup({
-            ensure_installed = {},
-            handlers = { -- this first function is the "default handler"
-            -- it applies to every language server without a "custom handler"
-            function(server_name)
-                require('lspconfig')[server_name].setup({})
-            end}
-        })
-    end
-}}
+            -- LspAttach is where you enable features that only work
+            -- if there is a language server active in the file
+            vim.api.nvim_create_autocmd('LspAttach', {
+                desc = 'LSP actions',
+                callback = function(event)
+                    -- Autoformat on save
+                    local id = vim.tbl_get(event, 'data', 'client_id')
+                    local client = id and vim.lsp.get_client_by_id(id)
+                    if client == nil then
+                        return
+                    end
 
+                    if client.supports_method('textDocument/formatting') then
+                        buffer_autoformat(event.buf)
+                    end
+
+                    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>',
+                        { buffer = event.buf, desc = "Hover over selection" })
+                    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>',
+                        { buffer = event.buf, desc = "Go to definition" })
+                    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>',
+                        { buffer = event.buf, desc = "Go to decleration" })
+                    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>',
+                        { buffer = event.buf, desc = "Go to implementation" })
+                    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>',
+                        { buffer = event.buf, desc = "Go to type definition" })
+                    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>',
+                        { buffer = event.buf, desc = "Show references" })
+                    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>',
+                        { buffer = event.buf, desc = "Show signature help" })
+                    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>',
+                        { buffer = event.buf, desc = "Rename" })
+                    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
+                        { buffer = event.buf, desc = "Format selection" })
+                    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+                        { buffer = event.buf, desc = "Code action" })
+                end
+            })
+
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = { -- this first function is the "default handler"
+                    -- it applies to every language server without a "custom handler"
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end }
+            })
+        end
+    } }
