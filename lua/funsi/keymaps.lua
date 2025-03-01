79cd3310ca538ce6062ca390eefde63a18052a31
Morphylus <martindefuns@kns.ch>
2025-03-01 15:38:07 +0100

    add tmux navigator for simple tmux navigation

--- lua/funsi/keymaps.lua
+++ lua/funsi/keymaps.lua
@@ -1,8 +1,8 @@
 -- Easier window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Telescope related
 local builtin = require('telescope.builtin')
@@ -10,5 +10,3 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-
