2dfa7434658fd02678e9ae28283e2b913e2018a1
Morphylus <martindefuns@kns.ch>
2025-05-10 09:31:25 +0200

    pin version of mason

--- lua/funsi/plugins/lsp.lua
+++ lua/funsi/plugins/lsp.lua
@@ -133,8 +133,10 @@ return { {
                 }
             })
 
+            require('mason').setup()
+
             require('mason-lspconfig').setup({
-                ensure_installed = { "eslint" },
+                ensure_installed = { "eslint", "clangd" },
                 handlers = { -- this first function is the "default handler"
                     -- it applies to every language server without a "custom handler"
                     function(server_name)
