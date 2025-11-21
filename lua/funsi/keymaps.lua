9112eda6be1a81386e5b4c38b909368eee70db00
Morphylus <martindefuns@kns.ch>
2025-11-21 19:04:13 +0100

    remove gpt and add show diagnostics

--- lua/funsi/keymaps.lua
+++ lua/funsi/keymaps.lua
@@ -25,18 +25,4 @@ vim.keymap.set("n", "<leader>i", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ 0 }), { 0 })
 end, { noremap = true, silent = true })
 
--- GPT Models
--- Both visual and normal mode for each, so you can open with a visual selection or without.
-vim.api.nvim_set_keymap('v', '<leader>a', ':GPTModelsCode<CR>', {
-    noremap = true
-})
-vim.api.nvim_set_keymap('n', '<leader>a', ':GPTModelsCode<CR>', {
-    noremap = true
-})
-
-vim.api.nvim_set_keymap('v', '<leader>c', ':GPTModelsChat<CR>', {
-    noremap = true
-})
-vim.api.nvim_set_keymap('n', '<leader>c', ':GPTModelsChat<CR>', {
-    noremap = true
-})
+vim.keymap.set('n', 'L', '<cmd>Lspsaga show_cursor_diagnostics<cr>', { desc = 'Show diagnostic' })
