fe578627e53d988d8bc082d0bfa5fcfb5e14cb2b
Martin Defuns <martin.defuns@smoca.ch>
2026-02-03 13:07:05 +0100

    add sourcekit support (uikit not working)

--- lua/funsi/plugins/lsp.lua
+++ lua/funsi/plugins/lsp.lua
@@ -168,9 +168,24 @@ return { {
                             },
                         })
                         vim.lsp.enable('lua_ls')
-                    end
-
+                    end,
                 }
+            })
+
+            -- sourcekit is NOT managed by mason, set up separately
+            local lspconfig = require('lspconfig')
+            lspconfig["sourcekit"].setup({
+                capabilities = capabilities,
+                cmd = {
+                    "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp",
+                },
+                root_dir = function(filename, _)
+                    local util = require("lspconfig.util")
+                    return util.root_pattern("buildServer.json")(filename)
+                        or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+                        or util.find_git_ancestor(filename)
+                        or util.root_pattern("Package.swift")(filename)
+                end,
             })
         end
     } }
